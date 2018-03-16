e9480ffb4de7263606c9b433e39e251dc6fa7a20
Code Brine <codebrineteam@gmail.com>
2018-03-16 12:42:06 +0600

    User Panel 90% done

--- user/userpanel.php
+++ user/userpanel.php
@@ -8,11 +8,45 @@
 					<?php echo flashmsg(); ?>
 				<br/>
 				
+				<?php
+				$userid = $_SESSION['userid'];
+									// dataquery 	
+									
+							
+				$query = dataquery($connection,"SELECT `users`.* FROM `users` WHERE `users`.id='$userid'");
+														
+					$row = mysqli_fetch_array($query,MYSQLI_ASSOC);
+					//pd($row);			
+														
+				?>
+				<table class="table">
+					<tr>
+						<td> Name : </td>  <td><?php echo $row['full_name'];?> </td>
+					</tr>
+					<tr>	
+						<td> Username : </td>  <td><?php echo $row['username'];?> </td>
+					</tr>
+					<tr>					
+						<td> Company Name : </td>  <td><?php echo $row['company_name'];?> </td>
+					</tr>
+					
+					<tr>
+						<td> Email : </td>  <td><?php echo $row['email'];?> </td>
+					</tr>	
+					<tr>	
+						<td> Contact No : </td>  <td><?php echo $row['contactno'];?> </td>
+					</tr>	
+					<tr>	
+						<td> Address : </td>  <td><?php echo $row['address'];?> </td>
+					</tr>
+					<tr>	
+						<td> Join Date : </td>  <td><?php echo date("d-M-Y H:i A",$row['updatetime']);?> </td>
+					</tr>	
+					
+
+				</table>	
 				
-				
-				<!--https://developers.google.com/chart/interactive/docs/gallery/piechart-->
-				 <div id="piechart" style="width: 900px; height: 500px;"></div>
-				<script type="text/javascript" src="../js/charts/loader.js"></script>
+			
 		</div>	
 	</div>	
 			
